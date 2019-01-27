834507ab5d97798d9cac2218d2f694f49560b072
onoxeve <onoxeve@gmail.com>
2019-01-27 20:55:09 +0900

    remove: FeaturedImage Components

--- src/components/Summary/index.js
+++ src/components/Summary/index.js
@@ -1,26 +1,19 @@
 import React from 'react';
 
-import FeaturedImage from '../FeaturedImage';
 import H1 from '../H1';
 import P from '../P';
 import Wrapper from './Wrapper';
 import Link from './Link';
 import Date from './Date';
-// import ContinueReading from './ContinueReading';
 
-function Summary({date, title, excerpt, slug, image}) {
+function Summary({date, title, excerpt, slug}) {
   return (
     <Wrapper>
-        {image &&
-          <Link to={slug}>
-            <FeaturedImage sizes={image.childImageSharp.sizes}/>
-          </Link>
-        }
-        <Link to={slug}>
-          <H1>{title}</H1>
-          <Date>{date}</Date>
-          <P>{excerpt}</P>
-        </Link>
+      <Link to={slug}>
+        <H1>{title}</H1>
+        <Date>{date}</Date>
+        <P>{excerpt}</P>
+      </Link>
     </Wrapper>
   );
 }
